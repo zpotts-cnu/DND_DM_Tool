65c3db5e88977b90b56d46bd3e58e36d315a5ed6
Zachary Potts <zachary.potts.19@cnu.edu>
2023-02-27 14:56:33 -0500

    Added utilities.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 
 using json = nlohmann::json;
 
-json open_json_file(std::string filename) {
+json open_json_file(std::string filename) {  // Moving to utilities.cpp
 	//const char* file = filename.c_str();
 	std::ifstream f(filename.c_str());
 	json data = json::parse(f);
