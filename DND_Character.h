9890076c32d50247a17430940c728c52f5f46db3
Zachary Potts <zachary.potts.19@cnu.edu>
2023-02-27 13:55:58 -0500

    Began adding member functions to DND_PC and added skills to ref_tables and constants

--- DND_Character.h
+++ DND_Character.h
@@ -9,9 +9,15 @@ public:
 	~DND_Character();
 	virtual std::string getName();
 	virtual std::string getRace();
+	virtual int getMaxHP();
+	virtual int getCurrentHP();
+	virtual int getArmorClass();
 	virtual int getStat(DND_STAT stat);
 protected:
 	std::string character_name;
 	DND_RACE character_race;
+	int max_hp;
+	int current_hp;
+	int ac;
 	std::map<DND_STAT, int> character_stats;
 };
--- DND_PC.h
+++ DND_PC.h
@@ -5,10 +5,20 @@ class DND_PC : DND_Character {
 public:
 	DND_PC();
 	~DND_PC();
+	// Base Class Methods
 	std::string getName();
 	std::string getRace();
+	int getMaxHP();
+	int getCurrentHP();
+	int getArmorClass();
+	int getStat(DND_STAT stat);
+	// PC Class Methods
 	std::string getClass();
+	int getLevel();
+	int getProfBonus();
 private:
 	DND_CLASS pc_class;
+	int pc_level;
+	int pc_proficiency_bonus;
 };
 
